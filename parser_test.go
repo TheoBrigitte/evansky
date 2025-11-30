c21c8780101291822eca30bbafe7ee1addd6edb1
Theo Brigitte <theo.brigitte@gmail.com>
2025-11-30 11:30:46 +0100

    add test case for season folder

--- parser_test.go
+++ parser_test.go
@@ -101,6 +101,7 @@ var testData = []string{
 	"1883 - S01E01 - 1883 WEBDL-1080p Proper.mkv",
 	"网球王子 - S01E172 - 第 172 集.mkv",
 	"Dragon Ball Z 1989-1996 [30Th Anniversary] REMASTERED Intégral (291EP) NC MULTI 1080p HDLight BluRay AC3 x265-Thebatou8652",
+	"Tortues Ninja - les chevaliers d’ecaille S01 VOF 720p HDTV AAC 2.0 x265-DeSs",
 }
 
 func TestParser(t *testing.T) {
