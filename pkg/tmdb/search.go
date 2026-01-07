6daa4228a51a24f17066719c2f457e9642d017cd
Theo Brigitte <theo.brigitte@gmail.com>
2026-01-07 04:28:30 +0100

    Re-query without year and language when receiving no results

--- pkg/tmdb/search.go
+++ pkg/tmdb/search.go
@@ -1,26 +1,29 @@
 package tmdb
 
 import (
+	"errors"
 	"fmt"
 	"slices"
 	"strconv"
 
-	"github.com/TheoBrigitte/evansky/pkg/provider"
 	gotmdb "github.com/cyruzin/golang-tmdb"
 	"github.com/rs/zerolog/log"
+
+	"github.com/TheoBrigitte/evansky/pkg/provider"
 )
 
 // SearchMovie search for movies using query and year (if provided).
 // see: https://developer.themoviedb.org/reference/search-movie
 func (c *Client) SearchMovie(req provider.Request) (provider.ResponseMovie, error) {
-	additionalQuery := buildAdditionalQuery(req)
-	log.Debug().Str("query", req.Query).Any("additionalQuery", additionalQuery).Msg("searching movie")
-	movies, err := c.client.GetSearchMovies(req.Query, additionalQuery)
-	if err != nil {
-		return nil, err
-	}
-	if len(movies.Results) <= 0 {
-		return nil, fmt.Errorf("%w: %w", provider.ErrNoResult, err)
+	movies, err := c.searchMovie(req)
+	if err != nil && errors.Is(err, provider.ErrNoResult) {
+		// Try again without year and language filters
+		req.Year = 0
+		req.Language = ""
+		movies, err = c.searchMovie(req)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	slices.SortStableFunc(movies.Results, func(e1, e2 gotmdb.MovieResult) int {
@@ -32,9 +35,44 @@ func (c *Client) SearchMovie(req provider.Request) (provider.ResponseMovie, erro
 	return c.newMovieResponse(movieByClosestYear(req.Year, movies.Results), req.Language)
 }
 
+func (c *Client) searchMovie(req provider.Request) (*gotmdb.SearchMovies, error) {
+	additionalQuery := buildAdditionalQuery(req)
+	log.Debug().Str("query", req.Query).Any("additionalQuery", additionalQuery).Msg("searching movie")
+	movies, err := c.client.GetSearchMovies(req.Query, additionalQuery)
+	if err != nil {
+		return nil, err
+	}
+	if len(movies.Results) <= 0 {
+		return nil, fmt.Errorf("%w: %w", provider.ErrNoResult, err)
+	}
+
+	return movies, nil
+}
+
 // SearchTV search for tv shows using query and year (if provided).
 // see: https://developer.themoviedb.org/reference/search-tv
 func (c *Client) SearchTV(req provider.Request) (provider.ResponseTV, error) {
+	tvshows, err := c.searchTV(req)
+	if err != nil && errors.Is(err, provider.ErrNoResult) {
+		// Try again without year and language filters
+		req.Year = 0
+		req.Language = ""
+		tvshows, err = c.searchTV(req)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	slices.SortStableFunc(tvshows.Results, func(e1, e2 gotmdb.TVShowResult) int {
+		e1Score := computePopularity(e1.Popularity, e1.VoteAverage, e1.VoteCount)
+		e2Score := computePopularity(e2.Popularity, e2.VoteAverage, e2.VoteCount)
+		return e2Score - e1Score
+	})
+
+	return c.newTVResponse(tvshowByClosestYear(req.Year, tvshows.Results), req)
+}
+
+func (c *Client) searchTV(req provider.Request) (*gotmdb.SearchTVShows, error) {
 	additionalQuery := buildAdditionalQuery(req)
 	log.Debug().Str("query", req.Query).Any("additionalQuery", additionalQuery).Msg("searching tv")
 	tvshows, err := c.client.GetSearchTVShow(req.Query, additionalQuery)
@@ -45,13 +83,7 @@ func (c *Client) SearchTV(req provider.Request) (provider.ResponseTV, error) {
 		return nil, fmt.Errorf("%w: %w", provider.ErrNoResult, err)
 	}
 
-	slices.SortStableFunc(tvshows.Results, func(e1, e2 gotmdb.TVShowResult) int {
-		e1Score := computePopularity(e1.Popularity, e1.VoteAverage, e1.VoteCount)
-		e2Score := computePopularity(e2.Popularity, e2.VoteAverage, e2.VoteCount)
-		return e2Score - e1Score
-	})
-
-	return c.newTVResponse(tvshowByClosestYear(req.Year, tvshows.Results), req)
+	return tvshows, nil
 }
 
 // searchMulti search for multi media using query.
