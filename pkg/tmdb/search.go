TheoBrigitte/evansky

commit baf20213ff0c2ca4c1cb1b6869af590e12229434

Replace TMDB client
- Replace github.com/cyruzin/golang-tmdb with github.com/golusoris/goenvoy/metadata

pkg/tmdb/search.go
@@ -3,8 +3,9 @@ package tmdb
 import (
 	"errors"
 	"strconv"
+	"strings"
 
-	gotmdb "github.com/cyruzin/golang-tmdb"
+	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 	"github.com/rs/zerolog/log"
 
 	"github.com/TheoBrigitte/evansky/pkg/provider"
@@ -29,10 +30,10 @@ func (c *Client) SearchMovie(req provider.Request) (provider.ResponseMovie, floa
 	return resp, score, err
 }
 
-func (c *Client) searchMovie(req provider.Request) (*gotmdb.SearchMovies, error) {
-	additionalQuery := buildAdditionalQuery(req)
-	log.Debug().Str("query", req.Query).Any("additionalQuery", additionalQuery).Msg("searching movie")
-	movies, err := c.client.GetSearchMovies(req.Query, additionalQuery)
+func (c *Client) searchMovie(req provider.Request) (*tmdb.PaginatedResult[tmdb.MovieResult], error) {
+	query := buildAdditionalQuery(req)
+	log.Debug().Str("query", query).Any("language", req.QueryLanguage).Msg("searching movie")
+	movies, err := c.client.SearchMovies(c.ctx, query, req.QueryLanguage, 1)
 	if err != nil {
 		return nil, err
 	}
@@ -62,10 +63,10 @@ func (c *Client) SearchTV(req provider.Request) (provider.ResponseTV, float64, e
 	return resp, score, err
 }
 
-func (c *Client) searchTV(req provider.Request) (*gotmdb.SearchTVShows, error) {
-	additionalQuery := buildAdditionalQuery(req)
-	log.Debug().Str("query", req.Query).Any("additionalQuery", additionalQuery).Msg("searching tv")
-	tvshows, err := c.client.GetSearchTVShow(req.Query, additionalQuery)
+func (c *Client) searchTV(req provider.Request) (*tmdb.PaginatedResult[tmdb.TVResult], error) {
+	query := buildAdditionalQuery(req)
+	log.Debug().Str("query", query).Any("language", req.QueryLanguage).Msg("searching tv")
+	tvshows, err := c.client.SearchTV(c.ctx, query, req.QueryLanguage, 1)
 	if err != nil {
 		return nil, err
 	}
@@ -76,21 +77,14 @@ func (c *Client) searchTV(req provider.Request) (*gotmdb.SearchTVShows, error) {
 	return tvshows, nil
 }
 
-func buildAdditionalQuery(req provider.Request) map[string]string {
-	additionalQuery := make(map[string]string)
+func buildAdditionalQuery(req provider.Request) string {
+	e := []string{req.Query}
 	if req.Year != 0 {
-		additionalQuery["year"] = strconv.Itoa(req.Year)
+		e = append(e, strconv.Itoa(req.Year))
 	}
-	if req.QueryLanguage != "" {
-		additionalQuery["language"] = req.QueryLanguage
-	}
-	return additionalQuery
+	return strings.Join(e, "&")
 }
 
-func buildLanguageQuery(language string) map[string]string {
-	additionalQuery := make(map[string]string)
-	if language != "" {
-		additionalQuery["language"] = language
-	}
-	return additionalQuery
+func buildLanguageQuery(language string) string {
+	return language
 }

go.mod
@@ -1,12 +1,13 @@
 module github.com/TheoBrigitte/evansky
 
-go 1.25.0
+go 1.26.1
 
 require (
 	github.com/abadojack/whatlanggo v1.0.1
 	github.com/adrg/strutil v0.3.1
-	github.com/cyruzin/golang-tmdb v1.9.2
 	github.com/gohugoio/hugo v0.160.1
+	github.com/golusoris/goenvoy/metadata v1.2.1
+	github.com/golusoris/goenvoy/metadata/video/tmdb v1.3.0
 	github.com/middelink/go-parse-torrent-name v0.0.0-20190301154245-3ff4efacd4c4
 	github.com/pemistahl/lingua-go v1.4.0
 	github.com/prometheus/common v0.67.5

go.sum
@@ -58,8 +58,6 @@ github.com/clipperhouse/uax29/v2 v2.7.0 h1:+gs4oBZ2gPfVrKPthwbMzWZDaAFPGYK72F0NJ
 github.com/clipperhouse/uax29/v2 v2.7.0/go.mod h1:EFJ2TJMRUaplDxHKj1qAEhCtQPW2tJSwu5BF98AuoVM=
 github.com/cpuguy83/go-md2man/v2 v2.0.6/go.mod h1:oOW0eioCTA6cOiMLiUPZOpcVxMig6NIQQ7OS05n1F4g=
 github.com/creack/pty v1.1.9/go.mod h1:oKZEueFk5CKHvIhNR5MUki03XCEU+Q6VDXinZuGJ33E=
-github.com/cyruzin/golang-tmdb v1.9.2 h1:8G9w16PgzYNkmkbT0Sdu/Asx+yeQ93rbp3kQUxBu+AE=
-github.com/cyruzin/golang-tmdb v1.9.2/go.mod h1:Yx4f4KyLgWAnvwgZ729nJPOTKkD4epYoK+cGDZ3AFzs=
 github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
 github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
 github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
@@ -104,6 +102,10 @@ github.com/gohugoio/hugo-goldmark-extensions/extras v0.7.0 h1:I/n6v7VImJ3aISLnn7
 github.com/gohugoio/hugo-goldmark-extensions/extras v0.7.0/go.mod h1:9LJNfKWFmhEJ7HW0in5znezMwH+FYMBIhNZ3VWtRcRs=
 github.com/gohugoio/hugo-goldmark-extensions/passthrough v0.5.0 h1:p13Q0DBCrBRpJGtbtlgkYNCs4TnIlZJh8vHgnAiofrI=
 github.com/gohugoio/hugo-goldmark-extensions/passthrough v0.5.0/go.mod h1:ob9PCHy/ocsQhTz68uxhyInaYCbbVNpOOrJkIoSeD+8=
+github.com/golusoris/goenvoy/metadata v1.2.1 h1:dGxC0Uw5BVeQjx2a2ESxqnlk9CVTo+lDoHfeCjKM4IA=
+github.com/golusoris/goenvoy/metadata v1.2.1/go.mod h1:KtSq+BmeP+71uanbw8zLP7dW89pBm360LPhd0VmeJZY=
+github.com/golusoris/goenvoy/metadata/video/tmdb v1.3.0 h1:L9ys1BlbZGKcnkGKpCLXCJy1RkfUMgDYKq9JAPSN1A0=
+github.com/golusoris/goenvoy/metadata/video/tmdb v1.3.0/go.mod h1:ZX3gtjLfS7+/zB7D27bOPCR90pMw4rFCFBvd7+sNZoc=
 github.com/google/go-cmp v0.3.1/go.mod h1:8QqcDgzrUqlUb/G2PQTWiueGozuR1884gddMywk6iLU=
 github.com/google/go-cmp v0.5.9/go.mod h1:17dUlkBOakJ0+DkrSSNjCkIjxS6bF9zb3elmeNGIjoY=
 github.com/google/go-cmp v0.7.0 h1:wk8382ETsv4JYUZwIsn6YpYiWiBsYLSJiTsyBybVuN8=

pkg/tmdb/client.go
@@ -2,6 +2,7 @@
 package tmdb
 
 import (
+	"context"
 	"crypto/sha1" //nolint:gosec
 	"fmt"
 	"net/http"
@@ -9,8 +10,9 @@ import (
 	"path/filepath"
 	"time"
 
-	gotmdb "github.com/cyruzin/golang-tmdb"
 	"github.com/gohugoio/hugo/cache/filecache"
+	"github.com/golusoris/goenvoy/metadata"
+	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 	"github.com/spf13/afero"
 	"github.com/spf13/pflag"
 
@@ -37,17 +39,14 @@ func New(flags *pflag.FlagSet) (provider.Interface, error) {
 		cd = filepath.Join(dir, defaultCacheDir)
 	}
 
-	tmdbClient, err := gotmdb.Init(apiKey)
-	if err != nil {
-		return nil, err
-	}
-	tmdbClient.SetClientConfig(newClient(&clientOptions{
+	tmdbClient := tmdb.New(apiKey, metadata.WithHTTPClient(newClient(&clientOptions{
 		cacheDir: cd,
 		ttl:      cacheTTL,
-	}))
+	})))
 
 	c := &Client{
 		client: tmdbClient,
+		ctx:    context.TODO(),
 	}
 
 	return c, nil
@@ -63,7 +62,7 @@ type clientOptions struct {
 }
 
 // newClient returns a new http.Client with caching capabilities if ttl > 0.
-func newClient(o *clientOptions) http.Client {
+func newClient(o *clientOptions) *http.Client {
 	var transport http.RoundTripper
 
 	if o != nil && o.ttl > 0 {
@@ -95,7 +94,7 @@ func newClient(o *clientOptions) http.Client {
 		transport = http.DefaultTransport
 	}
 
-	return http.Client{
+	return &http.Client{
 		Transport: transport,
 	}
 }

pkg/tmdb/score.go
@@ -4,7 +4,7 @@ import (
 	"math"
 )
 
-func computePopularity(popularity, voteAverage float32, voteCount int64) int {
+func computePopularity(popularity, voteAverage float64, voteCount int) int {
 	if voteCount == 0 {
 		return 0
 	}

pkg/tmdb/type_movie.go
@@ -4,7 +4,7 @@ import (
 	"math"
 	"time"
 
-	gotmdb "github.com/cyruzin/golang-tmdb"
+	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 	"github.com/rs/zerolog/log"
 
 	"github.com/TheoBrigitte/evansky/pkg/provider"
@@ -14,17 +14,17 @@ import (
 type movieResponse struct {
 	*movie
 	multi  map[string]*movie
-	client *gotmdb.Client
+	client *Client
 
 	provider.ResponseBaseMovie
 }
 
 type movie struct {
-	result      gotmdb.MovieResult
+	result      tmdb.MovieResult
 	releaseDate time.Time
 }
 
-func (c *Client) newMovieResponse(result gotmdb.MovieResult, lang string) (*movieResponse, error) {
+func (c *Client) newMovieResponse(result tmdb.MovieResult, lang string) (*movieResponse, error) {
 	m, err := newMovie(result)
 	if err != nil {
 		return nil, err
@@ -32,7 +32,7 @@ func (c *Client) newMovieResponse(result gotmdb.MovieResult, lang string) (*movi
 
 	multi := &movieResponse{
 		movie:             m,
-		client:            c.client,
+		client:            c,
 		ResponseBaseMovie: provider.NewResponseBaseMovie(),
 	}
 	multi.multi = map[string]*movie{
@@ -42,7 +42,7 @@ func (c *Client) newMovieResponse(result gotmdb.MovieResult, lang string) (*movi
 	return multi, nil
 }
 
-func newMovie(result gotmdb.MovieResult) (m *movie, err error) {
+func newMovie(result tmdb.MovieResult) (m *movie, err error) {
 	m = &movie{
 		result: result,
 	}
@@ -75,10 +75,10 @@ func (r movie) GetPopularity() int {
 	return computePopularity(r.result.Popularity, r.result.VoteAverage, r.result.VoteCount)
 }
 
-func movieByClosestYear(query string, year int, movies []gotmdb.MovieResult) (gotmdb.MovieResult, float64) {
+func movieByClosestYear(query string, year int, movies []tmdb.MovieResult) (tmdb.MovieResult, float64) {
 	var bestScore float64 = -1
 	var bestTitleScore float64 = 0
-	var closestMatch gotmdb.MovieResult
+	var closestMatch tmdb.MovieResult
 
 	for index, t := range movies {
 		var yearScore float64
@@ -134,13 +134,13 @@ func (m *movieResponse) InLanguage(req provider.Request) (provider.Response, err
 		m.movie = r
 	} else {
 		languageQuery := buildLanguageQuery(req.DestinationLanguage)
-		details, err := m.client.GetMovieDetails(m.GetID(), languageQuery)
+		details, err := m.client.client.GetMovie(m.client.ctx, m.GetID(), languageQuery)
 		if err != nil {
 			return nil, err
 		}
 
 		// TODO: fetch the movie details in newMovie, so we can also store the full details here
-		result := gotmdb.MovieResult{
+		result := tmdb.MovieResult{
 			ID:               details.ID,
 			Title:            details.Title,
 			OriginalTitle:    details.OriginalTitle,
@@ -152,7 +152,8 @@ func (m *movieResponse) InLanguage(req provider.Request) (provider.Response, err
 			Popularity:       details.Popularity,
 			Adult:            details.Adult,
 			Video:            details.Video,
-			VoteMetrics:      details.VoteMetrics,
+			VoteAverage:      details.VoteAverage,
+			VoteCount:        details.VoteCount,
 		}
 
 		movie, err := newMovie(result)

pkg/tmdb/type_tv.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	gotmdb "github.com/cyruzin/golang-tmdb"
+	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 	"github.com/rs/zerolog/log"
 
 	"github.com/TheoBrigitte/evansky/pkg/provider"
@@ -20,13 +20,13 @@ type tvResponse struct {
 }
 
 type tv struct {
-	result       gotmdb.TVShowResult
+	result       tmdb.TVResult
 	firstAirDate time.Time
 	// Language indexed seasons cache
 	seasons []provider.ResponseTVSeason
 }
 
-func (c *Client) newTVResponse(result gotmdb.TVShowResult, req provider.Request) (*tvResponse, error) {
+func (c *Client) newTVResponse(result tmdb.TVResult, req provider.Request) (*tvResponse, error) {
 	m := &tvResponse{
 		multi:          make(map[string]*tv),
 		client:         c,
@@ -41,7 +41,7 @@ func (c *Client) newTVResponse(result gotmdb.TVShowResult, req provider.Request)
 	return m, nil
 }
 
-func (m *tvResponse) init(result gotmdb.TVShowResult, req provider.Request) error {
+func (m *tvResponse) init(result tmdb.TVResult, req provider.Request) error {
 	m.tv = &tv{
 		result: result,
 	}
@@ -57,14 +57,14 @@ func (m *tvResponse) init(result gotmdb.TVShowResult, req provider.Request) erro
 	}
 
 	languageQuery := buildLanguageQuery(req.DestinationLanguage)
-	resp, err := m.client.client.GetTVDetails(m.GetID(), languageQuery)
+	resp, err := m.client.client.GetTV(m.client.ctx, m.GetID(), languageQuery)
 	if err != nil {
 		return err
 	}
 
 	seasons := make([]provider.ResponseTVSeason, 0, len(resp.Seasons))
 	for _, s := range resp.Seasons {
-		season, err := m.client.client.GetTVSeasonDetails(m.GetID(), s.SeasonNumber, languageQuery)
+		season, err := m.client.client.GetTVSeason(m.client.ctx, m.GetID(), s.SeasonNumber, languageQuery)
 		if err != nil {
 			return err
 		}
@@ -115,10 +115,10 @@ func (r tv) GetSeason(seasonNumber int) (provider.ResponseTVSeason, error) {
 	return nil, fmt.Errorf("season %d not found for show %d", seasonNumber, r.GetID())
 }
 
-func tvshowByClosestYear(query string, year int, tvshows []gotmdb.TVShowResult) (gotmdb.TVShowResult, float64) {
+func tvshowByClosestYear(query string, year int, tvshows []tmdb.TVResult) (tmdb.TVResult, float64) {
 	var bestScore float64 = -1
 	var bestTitleScore float64 = 0
-	var closestMatch gotmdb.TVShowResult
+	var closestMatch tmdb.TVResult
 
 	for index, t := range tvshows {
 		var yearScore float64
@@ -170,12 +170,12 @@ func (m *tvResponse) InLanguage(req provider.Request) (provider.Response, error)
 		m.tv = r
 	} else {
 		languageQuery := buildLanguageQuery(req.DestinationLanguage)
-		details, err := m.client.client.GetTVDetails(m.GetID(), languageQuery)
+		details, err := m.client.client.GetTV(m.client.ctx, m.GetID(), languageQuery)
 		if err != nil {
 			return nil, err
 		}
 
-		result := gotmdb.TVShowResult{
+		result := tmdb.TVResult{
 			ID:               details.ID,
 			Name:             details.Name,
 			OriginalName:     details.OriginalName,
@@ -186,7 +186,8 @@ func (m *tvResponse) InLanguage(req provider.Request) (provider.Response, error)
 			BackdropPath:     details.BackdropPath,
 			Popularity:       details.Popularity,
 			OriginCountry:    details.OriginCountry,
-			VoteMetrics:      details.VoteMetrics,
+			VoteAverage:      details.VoteAverage,
+			VoteCount:        details.VoteCount,
 		}
 
 		err = m.init(result, req)

pkg/tmdb/type_tvepisode.go
@@ -1,9 +1,10 @@
 package tmdb
 
 import (
+	"context"
 	"time"
 
-	gotmdb "github.com/cyruzin/golang-tmdb"
+	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 
 	"github.com/TheoBrigitte/evansky/pkg/provider"
 )
@@ -11,18 +12,19 @@ import (
 type tvEpisodeResponse struct {
 	*tvEpisode
 	multi  map[string]*tvEpisode
-	client *gotmdb.Client
+	client *tmdb.Client
+	ctx    context.Context
 
 	provider.ResponseBaseTVEpisode
 }
 
 type tvEpisode struct {
-	result  gotmdb.TVEpisodeDetails
+	result  tmdb.EpisodeDetails
 	airDate time.Time
 	season  provider.ResponseTVSeason
 }
 
-func (c *Client) newTVEpisodeResponse(result gotmdb.TVEpisodeDetails, season provider.ResponseTVSeason, lang string) (*tvEpisodeResponse, error) {
+func (c *Client) newTVEpisodeResponse(result tmdb.EpisodeDetails, season provider.ResponseTVSeason, lang string) (*tvEpisodeResponse, error) {
 	t, err := newTVEpisode(result, season)
 	if err != nil {
 		return nil, err
@@ -31,6 +33,7 @@ func (c *Client) newTVEpisodeResponse(result gotmdb.TVEpisodeDetails, season pro
 	m := &tvEpisodeResponse{
 		tvEpisode:             t,
 		client:                c.client,
+		ctx:                   c.ctx,
 		ResponseBaseTVEpisode: provider.NewResponseBaseTVEpisode(),
 	}
 	m.multi = map[string]*tvEpisode{
@@ -40,7 +43,7 @@ func (c *Client) newTVEpisodeResponse(result gotmdb.TVEpisodeDetails, season pro
 	return m, nil
 }
 
-func newTVEpisode(result gotmdb.TVEpisodeDetails, season provider.ResponseTVSeason) (t *tvEpisode, err error) {
+func newTVEpisode(result tmdb.EpisodeDetails, season provider.ResponseTVSeason) (t *tvEpisode, err error) {
 	t = &tvEpisode{
 		result: result,
 		season: season,
@@ -87,7 +90,7 @@ func (m *tvEpisodeResponse) InLanguage(req provider.Request) (provider.Response,
 		m.tvEpisode = r
 	} else {
 		languageQuery := buildLanguageQuery(req.DestinationLanguage)
-		details, err := m.client.GetTVEpisodeDetails(m.GetSeason().GetShow().GetID(), m.GetSeason().GetSeasonNumber(), m.GetID(), languageQuery)
+		details, err := m.client.GetTVEpisode(m.ctx, m.GetSeason().GetShow().GetID(), m.GetSeason().GetSeasonNumber(), m.GetID(), languageQuery)
 		if err != nil {
 			return nil, err
 		}

commit 87925c5dbb153468156aa9b5f2fc83b7620c36ab

Fix query regression for tmdb
- Query can only be a single string and not a url query joined with &
- This regression was introduced in baf20213f

pkg/tmdb/search.go
@@ -3,7 +3,6 @@ package tmdb
 import (
 	"errors"
 	"strconv"
-	"strings"
 
 	"github.com/golusoris/goenvoy/metadata/video/tmdb"
 	"github.com/rs/zerolog/log"
@@ -86,11 +85,11 @@ func (c *Client) searchTV(req provider.Request) (*tmdb.PaginatedResult[tmdb.TVRe
 }
 
 func buildAdditionalQuery(req provider.Request) string {
-	e := []string{req.Query}
+	q := req.Query
 	if req.Year != 0 {
-		e = append(e, strconv.Itoa(req.Year))
+		q += " " + strconv.Itoa(req.Year)
 	}
-	return strings.Join(e, "&")
+	return q
 }
 
 func buildLanguageQuery(language string) string {